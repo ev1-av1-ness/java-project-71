10bd7e4621cc9e7d33437eb59e3ec5e7d08ee3fb
Ev1-av1-ness <zonova.elizabeth@yandex.ru>
2023-06-18 12:58:26 +0300

    help output

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,38 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import java.util.concurrent.Callable;
+
+import static java.lang.System.out;
+import static java.lang.System.err;
+
+@CommandLine.Command(name = "gendiff",
+        mixinStandardHelpOptions = true,
+        version = "gendiff 1.0",
+        description = "Compares two configuration files and shows a difference.")
+public class App implements Callable<Integer> {
+
+    private static final int SUCCESS_EXIT_CODE = 0;
+    private static final int ERROR_EXIT_CODE = 1;
+
+    /**
+     * Computes a result, or throws an exception if unable to do so.
+     *
+     * @return computed result
+     */
+    @Override
+    public Integer call() {
+        try {
+            //
+        } catch (Exception e) {
+            err.println(e.getMessage());
+            return ERROR_EXIT_CODE;
+        }
+        return SUCCESS_EXIT_CODE;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
